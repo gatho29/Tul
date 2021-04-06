68a186850845e14106cf6d67dd93e4fd4cd8e4ad
Nell Guerra <guerranell25@gmail.com>
2021-04-05 19:21:19 -0500

    fix: proyect name in server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/proyecto-tul'));
+app.use(express.static(__dirname + '/dist/proyectoTul'));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/proyecto-tul/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/proyectoTul/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
